ba92d896e3dd87ec733e08673a7970e91563e779
Carlos Salazar <csalazart33@gmail.com>
2015-05-02 22:26:48 +0200

    Modificaciones Asicionales a los Scripts.   + Agregando un Header con Información  y contacto asi como la dirección del Github   + Agregado condicionales a las opciones.. si solicita el demo el directorio creado es symfony_demo

--- installSF.bat
+++ installSF.bat
@@ -1,6 +1,37 @@
+REM ## Script Realizado por Carlos A Salazar <csalazart33@gmail.com>
+REM ## Si deseas Contribuir puedes hacer un FORK y contribuir al respecto
+REM ## En Github https://github.com/csalazart/SF2ShortsCommands 
+
 @echo off
-@echo "Instalando Symfony 2 .."
+@echo =======> Script Batch por CAST<csalazart33@gmail.com> <=======
+@echo 
+@echo  =============================================================
+@echo  =                                                           =
+@echo  =              Iniciando Proyecto Symfony 2.x               =
+@echo  =                                                           =
+@echo  =============================================================
+@SET _PROYECTO=%1
 @echo php symfony %*
 @php symfony %* 
-@echo "Instalación Concluida Ahora Pasando Shorts Cuts a Directorio " %1
-@copy console.bat %1\
+
+if %_PROYECTO%==demo goto demo
+set mensaje=
+goto fin
+
+:demo
+@SET _PROYECTO=symfony_demo
+@echo Has Seleccionado la instalacion del DEMO de symfony 2
+@set mensaje=Ejecuta console server:run para Ejecutar la demo y empezar en http://localhost:8000 Empieza a aprender Symfony
+goto fin 
+
+:fin
+@echo %mensaje%
+@echo Instalación Concluida Ahora Copiando Shorts Cuts a Directorio  %_PROYECTO% 
+@copy console.bat %_PROYECTO%\
+@cd %_PROYECTO%\
+@echo Entrando al Directorio %_PROYECTO% 
+goto:eof
+REM Script 
+pause
+exit
+
