ef16a9e7ee3603ad76ddb67cded6ca82f4b631d6
Carlos A Salazar <csalazart33@gmail.com>
2016-05-04 23:38:50 +0200

    + Agregando parametro de instalación por escritura de comando. + ahora se puede ejecutar installSF Proyecto o bien solo InstallSF y luego colocar el nombre del proyecto.

--- installSF.cmd
+++ installSF.cmd
@@ -18,32 +18,42 @@ color 2F
 @echo installSF nombreProyecto 2.8
 @echo Si desea instalar el demo escriba "demo" como nombre de proyecto o descarguelo de https://github.com/symfony/symfony-demo
 @echo Si es lo que desea Hacer Presione cualquier Tecla de lo Contrario presione Cltr + C
-pause >null
+pause.
 if NOT EXIST symfony goto nosymfony
 
-SET _PROYECTO=%1
-:: @echo php symfony %*
-@php symfony new %* 
- 
+IF NOT %1.==. GOTO generar
+@echo Escriba el nombre del proyecto y la version ejemplo: "proyecto 2.8" o "demo" para cargar el demo de SF3
+SET /p _entrada_=
+::@echo /p _ver=
+SET _PROYECTO=%_entrada_%
+GOTO next
 
+:generar
+SET _PROYECTO=%1
+
+:next
 if %_PROYECTO%==demo goto demo
-set mensaje=
+@echo Generando symfony para %_PROYECTO% 
+@php symfony new %_PROYECTO%
 goto fin
 
 :demo
 @SET _PROYECTO=symfony_demo
+@echo Generando symfony para %_PROYECTO% 
+@php symfony new %_PROYECTO%
 @echo Has Seleccionado la instalacion del DEMO de symfony 2
 @set mensaje=Ejecuta console server:run para Ejecutar la demo y empezar en http://localhost:8000 Empieza a aprender Symfony
-goto fin 
+goto fin
 
 :fin
 @echo %mensaje%
 @echo Instalacion Concluida Ahora Copiando Shorts Cuts a Directorio  %_PROYECTO% 
-@copy console.bat %_PROYECTO%\
+@copy console.cmd %_PROYECTO%\
 @cd %_PROYECTO%\
 @echo Entrando al Directorio %_PROYECTO% 
 color 07
 pause
+goto fin2
 
 :nosymfony
 if EXIST symfony goto fin2
