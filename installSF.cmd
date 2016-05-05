3dabc5950b6efd677927d56a258b207686fa47b7
casalazar@ext.vectorsf.com <casalazar@ext.vectorsf.com>
2016-05-05 11:03:01 +0200

    + En installSF.cmd Windows:    * Se ha Actualizado la Instalación de la Versión demo ya que se ha eliminado del script de symfony ahora es por clone de git    * Actualizado para que cargue el demo por git solo si esta disponible el git

--- installSF.cmd
+++ installSF.cmd
@@ -15,12 +15,14 @@ color 2F
 @echo            =                                                           =
 @echo            =============================================================
 @echo. 
-@echo Para instalar symfony 3 escriba:
-@echo installSF nombreProyecto
-@echo para instalar symfony 2.8 escriba:
-@echo installSF nombreProyecto 2.8
-@echo Si desea instalar el demo escriba "demo" como nombre de proyecto o descarguelo de https://github.com/symfony/symfony-demo
-@echo Si es lo que desea Hacer Presione cualquier Tecla de lo Contrario presione Cltr + C
+@echo Puedes Utiliar este Script de manera Directa Sin el Menu:
+@echo .....Para iniciar un proyecto symfony 3 escribe: "installSF nombreProyecto"
+@echo.
+@echo .....Para iniciar un proyecto symfony 2.8 escribe: "installSF nombreProyecto 2.8"
+@echo.
+@echo .....Para Iniciar el proyecto demo  y clonar desde github escribe "installSF demogit" Necesitas git instalado o descarguelo de https://github.com/symfony/symfony-demo
+@echo.
+@echo ..Si deseas continuar con el Menu Interactivo Presione cualquier Tecla de lo Contrario presione Cltr + C
 if NOT EXIST symfony goto nosymfony
 @echo Actualizando Dependencias de symfony
 @php symfony self-update
@@ -36,18 +38,16 @@ IF NOT %1.==. GOTO generar
 @echo            =                                                           =
 @echo            =   1. Iniciar Proyecto SF 2.8.X LST                        =
 @echo            =   2. Iniciar Proyecto SF 3.X Last Release                 =
-@echo            =   3. Iniciar Proyecto SF 3.X Demo                         =
-@echo            =   4. Clonar Proyecto SF 3.X Demo (Necesita Git)           =
-@echo            =   5. Salir                                                =
+@echo            =   3. Iniciar Proyecto SF 3.X Demo Clone (Necesita Git)    =
+@echo            =   q. Salir                                                =
 @echo            =                                                           =
 @echo            =============================================================
 @echo. 
 SET /p _opcion=
 if %_opcion% ==1 goto capture
 if %_opcion% ==2 goto capture
-if %_opcion% ==3 goto demo
-if %_opcion% ==4 goto demogit
-if %_opcion% ==5 goto fin2
+if %_opcion% ==3 goto demogit
+if %_opcion% ==q goto fin2
 
 :capture
 @echo Ingresa el Nombre del Proyecto y presiona enter
@@ -60,8 +60,13 @@ if %_opcion% ==1 goto sf28
 if %_opcion% ==2 goto sf3 
 
 :generar
+if %1 == demogit goto demogit
 SET _PROYECTO=%1
-
+if %2.==. goto sf3
+REM genera proyecto 2.8 por entrada directa sin Menu 
+@php symfony new %_PROYECTO%
+@set ver=
+goto fin
 
 :sf28
 REM ::if %_PROYECTO%==demo goto demo
@@ -75,27 +80,19 @@ REM ::if %_PROYECTO%==demo goto demo
 @set ver=3
 goto fin
 
-:demo
-@SET _PROYECTO=symfony_demo
-@echo Generando symfony para %_PROYECTO% 
-@php symfony new demo
-@echo Has Seleccionado la instalacion del DEMO de symfony 2
-@set mensaje=Ejecuta el comando "console server:run" para Ejecutar la demo y empezar en http://localhost:8000 Empieza a aprender Symfony
-@set ver=3
-goto fin
-
 :demogit
 @SET _PROYECTO=symfony-demo
 @echo Generando symfony para %_PROYECTO% 
 @git clone https://github.com/symfony/symfony-demo.git
 @echo Has Seleccionado Clonar del repositorio el DEMO de symfony 3
 @set mensaje=Ejecuta el comando "console server:run" para Ejecutar la demo y empezar en http://localhost:8000 Empieza a aprender Symfony
+@set ver=3
 goto fin
 
 :fin
 @echo %mensaje%
 @echo Instalacion Concluida Ahora Copiando Shorts Cuts a Directorio  %_PROYECTO% 
-@copy console%ver%.cmd %_PROYECTO%\
+@copy console%ver%.cmd %_PROYECTO%\console.cmd
 @cd %_PROYECTO%\
 @echo Entrando al Directorio %_PROYECTO% 
 color 07
