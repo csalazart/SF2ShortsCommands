e8d410703d0bc4aac37e1d639b30357c68f0f49f
CAST <csalazart33@gmail.com>
2017-03-08 14:04:44 +0100

    Actualización de Comentarios y Créditos

--- console3.cmd
+++ console3.cmd
@@ -1,3 +1,7 @@
+REM :: ## Script Realizado por Carlos A Salazar <csalazart33@gmail.com>
+REM :: ## Si deseas Contribuir puedes hacer un FORK y contribuir al respecto
+REM :: ## En Github https://github.com/csalazart/SF2ShortsCommands 
+
 @echo off
 @echo "Ejecutando .."
 @echo php bin\console %*
