97bdd0ae62bb27148610071baf6321bb3d927153
Carlos <csalazar@paradigmadigital.com>
2018-06-21 17:55:17 +0200

    Modificaciones Version 1.3

---
+++ alias-console.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#  ## Script Realizado por Carlos A Salazar <csalazart33@gmail.com>
+#  ## Si deseas Contribuir puedes hacer un FORK y contribuir al respecto
+#  ## En Github https://github.com/csalazart/SF2ShortsCommands 
+
+echo "Creando Alias console..."
+echo "Ejecuta console + comandoSymfony"
+alias console="php app/console"
+
